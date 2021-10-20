7bc5bcebb4df42ef5f8d4d52c74a6058dcd6681b
RedOrange <gaolu0116@gmail.com>
2021-10-20 08:17:38 +0800

    add j

--- TestJava.java
+++ TestJava.java
@@ -1,6 +1,7 @@
 class TestJava{
 	public static void main(String[] args){
 		int i = 1;
+		int j = 1;
 		System.out.print("Hello World!!!!");
 		System.out.print("My name is Gaolu!");
 		System.out.print("I love you!");
