f2b283526df1fc9f10f5614fd880f6f28b23f492
RedOrange <gaolu0116@gmail.com>
2021-10-20 10:10:15 +0800

    haha

--- TestJava.java
+++ TestJava.java
@@ -1,9 +1,8 @@
-class TestJava{
+clast TestJava{
 	public static void main(String[] args){
 		int i = 1;
 		int j = 1;
 		int a = 1;
-		System.out.print("Hello World!!!!");
 		System.out.print("Hello World!!!!");
 		System.out.print("Hello World!!!!");
 	}
